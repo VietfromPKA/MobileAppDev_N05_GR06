31bd938266df585aa0672656266c0af882222177
Quocday1 <22010234@st.phenikaa-uni.edu.vn>
2025-01-13 14:37:33 +0700

    update

--- qly_chi_tieu/lib/home/home_main.dart
+++ qly_chi_tieu/lib/home/home_main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:qly_chi_tieu/home/User_NguyenDucQuoc/DucQuoc-Screen.dart';
 import 'User_NguyenDucQuoc/apptest.dart';
-=======
-import 'package:qly_chi_tieu/home/DucQuoc-Screen.dart';
-import 'package:qly_chi_tieu/home/VanDuong_Screen.dart';
->>>>>>> 1525ff9a7e8c0ea59d2a0d2964addc452131970d
 import 'input_screen.dart';
 import 'calendar_screen.dart';
 import 'report_screen.dart';
@@ -24,16 +19,12 @@ class _ExpenseManagerAppState extends State<ExpenseManagerApp> {
   int _currentIndex = 0;
 
   final List<Widget> _screens = [
+    const UserInputScreen(),
     const InputScreen(),
     const CalendarScreen(),
     const ReportScreen(),
-    const UserInputScreen(),
     DucQuocScreen(),
-<<<<<<< HEAD
-    AppTestScreen(),
-=======
-    VanDuongScreen(),
->>>>>>> 1525ff9a7e8c0ea59d2a0d2964addc452131970d
+    const AppTestScreen(),
   ];
 
   @override
@@ -53,7 +44,7 @@ class _ExpenseManagerAppState extends State<ExpenseManagerApp> {
           selectedItemColor: Colors.teal,
           unselectedItemColor: Colors.grey,
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'Nhập'),
+            BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'Việt'),
             BottomNavigationBarItem(
                 icon: Icon(Icons.calendar_today), label: 'Lịch'),
             BottomNavigationBarItem(
@@ -63,11 +54,7 @@ class _ExpenseManagerAppState extends State<ExpenseManagerApp> {
             BottomNavigationBarItem(
                 icon: Icon(Icons.more_horiz), label: 'Đức Quốc'),
             BottomNavigationBarItem(
-<<<<<<< HEAD
                 icon: Icon(Icons.more_horiz), label: 'Review Bánh'),
-=======
-                icon: Icon(Icons.person_2_outlined), label: 'Văn Dương'),
->>>>>>> 1525ff9a7e8c0ea59d2a0d2964addc452131970d
           ],
         ),
       ),
